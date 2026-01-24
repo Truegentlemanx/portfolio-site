7278fef7e80e76ec286d439deb1d066fd4b6c2c7
dominik <dominik@protonmail.com>
2026-01-24 14:58:41 +0100

    Added a new model, updated ResumeItem model

--- projects/models.py
+++ projects/models.py
@@ -24,14 +24,30 @@ class ProjectImage(models.Model):
     def __str__(self):
         return f"{self.project.title} image"
     
+class Category(models.Model):
+    """Class representing a category for resume items."""
+    name = models.CharField(max_length=100, unique=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return self.name
+    
 class ResumeItem(models.Model):
     """Class representing resume items like education and employers"""
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name="resume_items",
+                                 on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
+    position = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["position"]
 
     def __str__(self):
         return f"Title: {self.title} Category: {self.category}"
