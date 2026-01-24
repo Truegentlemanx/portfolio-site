1f7d50290b6089a64148012701d3bffe277171b4
dominik <dominik@protonmail.com>
2026-01-24 15:02:24 +0100

    registered a new model and added custom admin classes for ResumeItem and Category

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from . models import Project, ProjectImage, ResumeItem
+from .models import Project, ProjectImage, ResumeItem, Category
 
-# Register your models here.
 admin.site.register(Project)
 admin.site.register(ProjectImage)
-admin.site.register(ResumeItem)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "order")
+    ordering = ("order",)
+    search_fields = ("name",)
+
+@admin.register(ResumeItem)
+class ResumeItemAdmin(admin.ModelAdmin):
+    list_display = ("category", "position", "title")
+    list_editable = ("position",)
+    list_filter = ("category",)
+    ordering = ("category__order", "position", "id")
